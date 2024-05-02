5221061a24d91257b0024cf6736f083e1dc7e1b7
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-05-02 15:47:22 +0700

    setting up project with navbar with styling

---
+++ app/Navbar.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import { AiFillBug } from "react-icons/ai"
+import { usePathname } from 'next/navigation'
+import classnames from 'classnames';
+
+const Navbar = () => {
+
+  const currentPath = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' }
+  ]
+
+  return (
+    <nav className="flex space-x-4">
+      <Link href="/"><AiFillBug /></Link>
+      <ul className='flex space-x-8'>
+        {links.map((link) => {
+          return <li key={link.href}><Link href={link.href} className={classnames({
+            'text-slate-900' : currentPath === link.href,
+            'text-slate-500' : currentPath !== link.href,
+            'hover:text-slate-900' : true
+          })}>{link.label}</Link></li>
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/issues/page.tsx
@@ -0,0 +1,13 @@
+"use client "
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const IssuePage = () => {
+
+
+  return (
+    <div>IssuePage</div>
+  )
+}
+
+export default IssuePage
