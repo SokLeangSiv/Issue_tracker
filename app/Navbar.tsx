a7cf8b993f051070733f534d9420412643b54e26
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-05-14 15:56:45 +0700

    add dropdown menu to show current user

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,37 +5,64 @@ import { AiFillBug } from "react-icons/ai"
 import { usePathname } from 'next/navigation'
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react'
-import { Box } from '@radix-ui/themes'
+import { Avatar, Box, DropdownMenu, Flex, Text } from '@radix-ui/themes'
 
 const Navbar = () => {
 
   const currentPath = usePathname();
 
-  const {data : session, status} = useSession();
+  const { data: session, status } = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' }
   ]
 
-  return (
-    <nav className="flex space-x-4 p-5 border-b-2 border-b-slate-500 mb-7">
-      <Link href="/"><AiFillBug /></Link>
-      <ul className='flex space-x-8'>
-        
-        {links.map((link) => {
-          return <li key={link.href}><Link href={link.href} className={classnames({
-            'text-slate-900' : currentPath === link.href,
-            'text-slate-500' : currentPath !== link.href,
-            'hover:text-slate-900' : true
-          })}>{link.label}</Link></li>
-        })}
-      </ul>
+  console.log(session)
 
-      <Box>
-        {status === 'authenticated' && <Link href='/api/auth/signout'>Log out</Link>}
-        {status === 'unauthenticated' && <Link href='/api/auth/signin'>Log in</Link>}
-      </Box>
+  return (
+    <nav className=" p-5 border-b-2 border-b-slate-500 mb-7">
+
+      <Flex justify="between">
+        <Flex align="center" gap='3'>
+          <Link href="/"><AiFillBug /></Link>
+          <ul className='flex space-x-8'>
+
+            {links.map((link) => {
+              return <li key={link.href}><Link href={link.href} className={classnames({
+                'text-slate-900': currentPath === link.href,
+                'text-slate-500': currentPath !== link.href,
+                'hover:text-slate-900': true
+              })}>{link.label}</Link></li>
+            })}
+          </ul>
+        </Flex>
+
+        <Box>
+          {status === 'authenticated' &&
+
+            (<DropdownMenu.Root >
+              <DropdownMenu.Trigger>
+                <Avatar src={session.user!.image!} fallback="?" size="2" radius='full'  className="cursor-pointer" />
+              </DropdownMenu.Trigger>
+
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size='2'>{session.user!.email}</Text>
+                </DropdownMenu.Label>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+             )
+            // <Link href='/api/auth/signout'>Log out</Link>
+          }
+          {status === 'unauthenticated' && <Link href='/api/auth/signin'>Log in</Link>}
+        </Box>
+
+      </Flex>
+
+
+
+
     </nav>
   )
 }
