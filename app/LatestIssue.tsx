ad5b930475286aba25b9315386fd1e827ef0d414
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-05-22 16:39:35 +0700

    build the latest isssue component

---
+++ app/LatestIssue.tsx
@@ -0,0 +1,57 @@
+import { Issue } from '@prisma/client';
+import { Avatar, Card, Flex, Heading, Table } from '@radix-ui/themes'
+import prisma from '@/prisma/client';
+import React from 'react'
+import Link from 'next/link';
+import IssueStatus from './issues/list/IssueStatus';
+import { ShowBageStatus } from '@/components';
+
+
+
+const LatestIssue = async () => {
+
+    const issues = await prisma.issue.findMany({
+        take: 5,
+        orderBy: {
+            created_at: 'desc'
+        },
+
+        include:{
+            assignToUser:true  // use this for call the user image when we call the issue that we have relation them in schema like relationship
+        }
+    })
+
+    return (
+        <Card>
+            <Heading size='4' mb='5'>Latest Issues</Heading>
+            <Table.Root>
+                <Table.Body>
+                    {issues.map(issue => {
+                        return (
+                            <Table.Row key={issue.id}>
+                                <Table.Cell>
+                                    <Flex justify='between'>
+                                        <Flex direction="column" align='start' gap='2'>
+
+                                            <Link href={`/issues/${issue.id}`}>
+                                                {issue.title}
+
+                                            </Link>
+
+                                            <ShowBageStatus status={issue.status} />  
+                                        </Flex>
+
+                                        {issue.assignToUser && <Avatar src={issue.assignToUser.image!}  alt='image'  fallback="?" />}  
+                                    </Flex>
+                                </Table.Cell>
+
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+            </Table.Root>
+        </Card>
+    )
+}
+
+export default LatestIssue
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Pagination from "@/components/Pagination";
-import Image from "next/image";
+import LatestIssue from "./LatestIssue";
+
 
 export default function Home({searchParams} : {searchParams : {page : string}}) {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+    <LatestIssue />
   );
 }
