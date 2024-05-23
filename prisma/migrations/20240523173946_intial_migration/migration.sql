a61902e7a25ad2c1f689ad4ca39b82d5615e63c6
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-05-24 01:07:19 +0700

    update

--- prisma/migrations/20240523173946_intial_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Issue" DROP CONSTRAINT "Issue_assignToUserId_fkey";
-
--- DropForeignKey
-ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "sessions" DROP CONSTRAINT "sessions_user_id_fkey";
